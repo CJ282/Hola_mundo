471a221b7a1f510fe72e12d00cd128093efb6358
CJ282 <cristianmartinezCJMR@gmail.com>
2021-07-12 11:07:57 -0500

    Primer commit (Hola mundo)

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int entero=3;
+        System.out.println(entero);
+        float decimal=2.5f;
+        System.out.println(decimal);
+        boolean booleano=true;
+        System.out.println(booleano);
+        String palabra="Cristian";
+        System.out.println(palabra);
+        byte edad =56;
+        System.out.println(edad);
+        double promedio=164.88;
+        System.out.println(promedio);
+    }
+}
---
+++ src/SegundaClase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SegundaClase {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner= new Scanner(System.in);
+        int numero = scanner.nextInt();
+        int resultado = CalculoDigitos(numero);
+        tem.out.println("El número de digitos es "+resultado)
+    }
+        public static int CalculoDigitos(int numero){
+        int cifras=0;
+        while(numero !=0);
+        
+    }
+}
